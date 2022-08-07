cf272434cb3e15e9666a8d0c1bc2c5e5b11155d7
marcello <marcelloavila@gmail.com>
2022-08-06 19:31:06 -0600

    test: add test skeleton

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func Hello() string {
+	return "Hello, world"
+}
 
 func main() {
-	log.Println("hi")
+	// get file from terminal
+	inputFile := os.Args[1]
+	// declare chunk size
+	maxSz, _ := strconv.Atoi(os.Args[2])
+	// read the whole content of file and pass it to file variable, in case of error pass it to err variable
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Could not open the file due to this %s error \n", err)
+	}
+	defer file.Close()
+
+	// create buffer
+	b := make([]byte, maxSz)
+
+	for {
+		// read content to buffer
+		readTotal, err := file.Read(b)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+		fileContent := string(b[:readTotal])
+		// print content from buffer
+		fmt.Println(fileContent)
+	}
 }
+
+// SOURCE: https://www.golinuxcloud.com/golang-read-a-file-methods/
